25cef5e01d1c2f8456d6c1e9b3074ee8673feac5
Rijensh Limbu <r0884125@ucll.be>
2025-05-28 15:21:54 +0200

    commit v

--- PokerIoT_Player/src/config.cpp
+++ PokerIoT_Player/src/config.cpp
@@ -1,10 +1,10 @@
 #include "config.h"
 #include "stdint.h"
 
-const char *ssid = "VICTUS-LIMBU 2508";
-const char *password = "6p{02U95";
-const char *host = "10.25.238.117";
+const char *ssid = "ucll-integratieproject";
+const char *password = "aesheiSei8uaMiag";
+const char *host = "team5-backend-itip-project5.apps.okd.ucll.cloud";
 const char *path = "/?type=esp32";
-const uint16_t port = 3000;
+const uint16_t port = 32685;
 
 bool actionTaken = false;
